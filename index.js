25a1fc56f9ac9cd078384b70effa304c174767cd
User <kai.luessow@fau.de>
2023-07-19 13:32:40 +0200

    initial implementation

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+const multer  = require('multer')
+const upload = multer({ dest: 'uploads/' })
 
 var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
+
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
 
+app.post('/api/fileanalyse/', upload.single('upfile'), (req, res) => {
+  try {
+    res.json({
+      name: req.file.originalname,
+      type: req.file.mimetype,
+      size: req.file.size
+    });
+  } catch (err) {
+    res.status(500).json({error: err}); 
+  }
+  
 
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
